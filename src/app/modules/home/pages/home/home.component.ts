1320b99067333275dbaf4bab49db5eb873343f9d
milonroy26 <milonchandro35@gmail.com>
2023-04-08 10:50:27 +0600

    json data add

--- src/app/modules/home/pages/home/home.component.ts
+++ src/app/modules/home/pages/home/home.component.ts
@@ -7,4 +7,37 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  public name:any;
+  public fnds:string[] = ['santo', 'krisno', 'amith', 'soikot'];
+  public fndsName:any;
+  public parseName:any;
+
+  public users:{} = [
+    { "id": 1,
+    "name": "Leanne Graham",
+    "username": "Bret",
+    "email": "Sincere@april.biz",
+    }
+  ]
+
+  constructor() { }
+
+  ngOnInit(): void {
+    // set item
+    localStorage.setItem('name', 'milon');
+    localStorage.setItem('id', '293');
+    // get item
+    this.name = localStorage.getItem('name');
+    // remove item
+    localStorage.removeItem('id');
+    // set json data
+    localStorage.setItem('users', JSON.stringify(this.users))
+  }
+
+  callFnds(){
+    localStorage.setItem('frinds', JSON.stringify(this.fnds));
+    this.fndsName = localStorage.getItem('frinds');
+    this.parseName = JSON.parse(this.fndsName);
+  }
+
 }
